70e865d876269a73a0e1bfe391a63e67da29df8d
dteran24 <dteran21@yahoo.com>
2023-04-13 22:18:06 -0500

    optimize validations and dropdown menu

--- my-app/src/components/SignupForm.tsx
+++ my-app/src/components/SignupForm.tsx
@@ -1,173 +1,278 @@
+/* eslint-disable no-useless-escape */
 import { useState } from "react";
 import { Form, Button, Dropdown, DropdownButton } from "react-bootstrap";
-import { FormData } from '../models/formData';
-import { v4 as uuidv4 } from 'uuid';
-
+import { FormData } from "../models/formData";
+import { v4 as uuidv4 } from "uuid";
 
 function SignupForm() {
   let uniqueID: string = uuidv4();
   const [validated, setValidated] = useState(false);
-  
+
   const [formData, setFormData] = useState<FormData>({
-    registrationID:'',
-    parentName: '',
-    studentName: '',
-    studentRegisterNumber: '',
-    address: '',
-    city:'',
-    zipCode: '',
-    country: '',
-    state:'',
-    emailAddress: '',
-    primaryContactPerson: '',
-    primaryContactMobile: '',
-    secondaryContactPerson:'',
-    secondaryContactMobile: '',
-  })
-  const [validateInputs, setValidateInputs] = useState({
-    parentName: false,
-    studentName: false,
-    studentRegisterNumber: false,
-    zipCode: false,
-    city: false,
-    primaryContactMobile: false,
-    secondaryContactMobile: false,
-  })
-  
-  const validateUserInputs = () => {
-    let validParentName = !/^[a-zA-Z\s]+$/.test(formData.parentName);
-    let validStudentName = !/^[a-zA-Z\s]+$/.test(formData.studentName);
-    let validRegisterNumber = !/^R-[a-zA-Z0-9]{3}-\.[a-zA-Z0-9]{3}$/.test(formData.studentRegisterNumber);
-    let validZip = !/^\d{6}$/.test(formData.zipCode);
-    let validCity = !/^[a-zA-Z]+$/.test(formData.city);
-    let validMobilePrimary = !/^\d{10}$/.test(formData.primaryContactMobile);
-    let validMobileSecondary = !/^\d{10}$/.test(formData.secondaryContactMobile);
-    setValidateInputs({
-      ...validateInputs,
-      parentName: validParentName,
-      studentName: validStudentName,
-      studentRegisterNumber: validRegisterNumber,
-      zipCode: validZip,
-      city: validCity,
-      primaryContactMobile: validMobilePrimary,
-      secondaryContactMobile: validMobileSecondary
-    })
+    registrationID: "",
+    parentName: "",
+    studentName: "",
+    studentRegisterNumber: "",
+    address: "",
+    city: "",
+    zipCode: "",
+    country: "",
+    state: "",
+    emailAddress: "",
+    primaryContactPerson: "",
+    primaryContactMobile: "",
+    secondaryContactPerson: "",
+    secondaryContactMobile: "",
+  });
+  // const [validateInputs, setValidateInputs] = useState({
+  //   parentName: false,
+  //   studentName: false,
+  //   studentRegisterNumber: false,
+  //   zipCode: false,
+  //   city: false,
+  //   primaryContactMobile: false,
+  //   secondaryContactMobile: false,
+  // })
 
+  const nameValidation = "^[a-zA-Zs]+$";
+  const registerNumberValidation = "^R-[a-zA-Z0-9]{3}-.[a-zA-Z0-9]{3}$";
+  const emailValidation = "^[^s@]+@[^s@]+.[^s@]+$";
+  const zipValidation = "^\\d{6}$";
+  const cityValidation = "^[a-zA-Z]+$";
+  const mobileValidation = "^\\d{10}$";
 
-}
-
+  //   const validateUserInputs = () => {
+  //     let validParentName = !/^[a-zA-Z\s]+$/.test(formData.parentName);
+  //     let validStudentName = !/^[a-zA-Z\s]+$/.test(formData.studentName);
+  //     let validRegisterNumber = !/^R-[a-zA-Z0-9]{3}-\.[a-zA-Z0-9]{3}$/.test(formData.studentRegisterNumber);
+  //     let validZip = !/^\d{6}$/.test(formData.zipCode);
+  //     let validCity = !/^[a-zA-Z]+$/.test(formData.city);
+  //     let validMobilePrimary = !/^\d{10}$/.test(formData.primaryContactMobile);
+  //     let validMobileSecondary = !/^\d{10}$/.test(formData.secondaryContactMobile);
+  //     setValidateInputs({
+  //       ...validateInputs,
+  //       parentName: validParentName,
+  //       studentName: validStudentName,
+  //       studentRegisterNumber: validRegisterNumber,
+  //       zipCode: validZip,
+  //       city: validCity,
+  //       primaryContactMobile: validMobilePrimary,
+  //       secondaryContactMobile: validMobileSecondary
+  //     })
+  // }
 
   const disableDropDownHandler = () => {
-    if (formData.country === '' || formData.country == null) {
+    if (formData.country === "" || formData.country == null) {
       return true;
     } else {
       return false;
     }
-  }
+  };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData({ ...formData, [event.target.id]: event.target.value })
-  }
-  
+    setFormData({ ...formData, [event.target.id]: event.target.value });
+  };
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    const form = event.currentTarget;
     event.preventDefault();
-    setValidated(true);
-    validateUserInputs();
-    
-    if (!Object.values(formData).every(property => property === null || property === '') && Object.values(validateInputs).every(property => property === true)) {
+    event.stopPropagation();
+    if (form.checkValidity() === false) {
+      console.log("inside false");
+    } else {
       setFormData({ ...formData, registrationID: uniqueID });
-      console.log("form was submitted")
-    } 
-    
-  }
-  
-  
+      console.log("form was submitted");
+    }
+
+    setValidated(true);
+  };
+
+  console.log("data", formData);
+
   return (
     <div className="d-flex justify-content-center">
-      <Form className="w-50" noValidate validated={validated} onSubmit={handleSubmit}>
+      <Form
+        className="w-50"
+        noValidate
+        validated={validated}
+        onSubmit={handleSubmit}
+      >
         <Form.Group className="mb-3" controlId="parentName">
           <Form.Label>Parent Name</Form.Label>
-          <Form.Control required type="text" value={formData.parentName} onChange={handleChange} isInvalid={validateInputs.parentName} />
-          <Form.Control.Feedback type="invalid">Please enter a valid name</Form.Control.Feedback>
+          <Form.Control
+            required
+            pattern={nameValidation}
+            type="text"
+            value={formData.parentName}
+            onChange={handleChange}
+          />
+          <Form.Control.Feedback type="invalid">
+            Please enter a valid name
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="studentName">
           <Form.Label>Student Name</Form.Label>
-          <Form.Control required type="text" value={formData.studentName} onChange={handleChange} isInvalid={validateInputs.studentName} />
-          <Form.Control.Feedback type="invalid">Please enter a valid name</Form.Control.Feedback>
+          <Form.Control
+            required
+            pattern={nameValidation}
+            type="text"
+            value={formData.studentName}
+            onChange={handleChange}
+          />
+          <Form.Control.Feedback type="invalid">
+            Please enter a valid name
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="studentRegisterNumber">
           <Form.Label>Student Register Number</Form.Label>
-          <Form.Control required type="text" placeholder="R-xxx.xxx" value={formData.studentRegisterNumber} onChange={handleChange} isInvalid={validateInputs.studentRegisterNumber} />
-          <Form.Control.Feedback type="invalid">Please enter a valid format</Form.Control.Feedback>
+          <Form.Control
+            required
+            pattern={registerNumberValidation}
+            type="text"
+            placeholder="R-xxx.xxx"
+            value={formData.studentRegisterNumber}
+            onChange={handleChange}
+          />
+          <Form.Control.Feedback type="invalid">
+            Please enter a valid format
+          </Form.Control.Feedback>
         </Form.Group>
         <div>
           <Form.Group className="mb-3" controlId="address">
             <Form.Label>Student Address</Form.Label>
-            <Form.Control required type="text" value={formData.address} onChange={handleChange}/>
+            <Form.Control
+              required
+              type="text"
+              value={formData.address}
+              onChange={handleChange}
+            />
           </Form.Group>
           <div className="d-flex flex-row justify-content-between">
             <Form.Group className="mb-3 w-50" controlId="city">
-              <Form.Control required type="text" placeholder="City" value={formData.city} onChange={handleChange} isInvalid={validateInputs.city} />
-              <Form.Control.Feedback type="invalid">Please enter a city</Form.Control.Feedback>
+              <Form.Control
+                required
+                pattern={cityValidation}
+                type="text"
+                placeholder="City"
+                value={formData.city}
+                onChange={handleChange}
+              />
+              <Form.Control.Feedback type="invalid">
+                Please enter a city
+              </Form.Control.Feedback>
             </Form.Group>
             <Form.Group className="mb-3" controlId="zipCode">
-              <Form.Control required type="text" placeholder="Zip/Postal" value={formData.zipCode} onChange={handleChange} isInvalid={validateInputs.zipCode} />
-              <Form.Control.Feedback type="invalid">Please enter a 6-digit zip code</Form.Control.Feedback>
+              <Form.Control
+                required
+                pattern={zipValidation}
+                type="text"
+                placeholder="Zip/Postal"
+                value={formData.zipCode}
+                onChange={handleChange}
+              />
+              <Form.Control.Feedback type="invalid">
+                Please enter a 6-digit zip code
+              </Form.Control.Feedback>
             </Form.Group>
           </div>
           <div className="d-flex flex-row justify-content-between">
             <Form.Group className="mb-3 w-75" controlId="country">
-              <Form.Control required type="text" placeholder="Country" value={formData.country} onChange={handleChange} />
+              <Form.Control
+                required
+                type="text"
+                placeholder="Country"
+                value={formData.country}
+                onChange={handleChange}
+              />
             </Form.Group>
             
-            <DropdownButton
-              disabled={disableDropDownHandler()}
-              title='State'
-              id='state'
-              onSelect={(e) => setFormData({...formData, state: e})}
-          >
-            <Dropdown.Item eventKey="Texas">Texas</Dropdown.Item>
-            <Dropdown.Item eventKey="California">California</Dropdown.Item>
-            <Dropdown.Item eventKey="Washington">Washington</Dropdown.Item>
-          </DropdownButton>
+            <Form.Group className="mb-3" controlId="state">
+              <Form.Control
+                required
+                disabled={disableDropDownHandler()}
+                as='select'
+                type="select"
+                placeholder="Country"
+                onChange={handleChange}
+              >
+                <option value="">State</option>
+                <option value="cash">Cash</option>
+                <option value="online">Online</option>
+                <option value="card_machine">Card Machine</option>
+              </Form.Control>
+            </Form.Group>
           </div>
         </div>
 
         <Form.Group className="mb-3" controlId="emailAddress">
           <Form.Label>Email address</Form.Label>
-          <Form.Control required type="email" placeholder="example@gmail.com" value={formData.emailAddress} onChange={handleChange}/>
-          <Form.Control.Feedback type="invalid">Please enter a valid email</Form.Control.Feedback>
+          <Form.Control
+            required
+            pattern={emailValidation}
+            type="email"
+            placeholder="example@gmail.com"
+            value={formData.emailAddress}
+            onChange={handleChange}
+          />
+          <Form.Control.Feedback type="invalid">
+            Please enter a valid email
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="primaryContactPerson">
           <Form.Label>Primary Contact</Form.Label>
-          <Form.Control required type="text" value={formData.primaryContactPerson} onChange={handleChange} />
+          <Form.Control
+            required
+            type="text"
+            value={formData.primaryContactPerson}
+            onChange={handleChange}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="primaryContactMobile">
           <Form.Label>Primary Contact Mobile</Form.Label>
-          <Form.Control required type="text" isInvalid={validateInputs.primaryContactMobile} value={formData.primaryContactMobile} onChange={handleChange}/>
-          <Form.Control.Feedback type="invalid">Please enter a 10-digit phone number</Form.Control.Feedback>
+          <Form.Control
+            required
+            pattern={mobileValidation}
+            type="text"
+            value={formData.primaryContactMobile}
+            onChange={handleChange}
+          />
+          <Form.Control.Feedback type="invalid">
+            Please enter a 10-digit phone number
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="secondaryContactPerson">
           <Form.Label>Secondary Contact</Form.Label>
-          <Form.Control required type="text" value={formData.secondaryContactPerson} onChange={handleChange}/>
+          <Form.Control
+            required
+            type="text"
+            value={formData.secondaryContactPerson}
+            onChange={handleChange}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="secondaryContactMobile">
           <Form.Label>Secondary Contact Mobile</Form.Label>
-          <Form.Control required type="text" value={formData.secondaryContactMobile} isInvalid={validateInputs.secondaryContactMobile} onChange={handleChange}/>
-          <Form.Control.Feedback type="invalid">Please enter a 10-digit phone number</Form.Control.Feedback>
+          <Form.Control
+            required
+            pattern={mobileValidation}
+            type="text"
+            value={formData.secondaryContactMobile}
+            onChange={handleChange}
+          />
+          <Form.Control.Feedback type="invalid">
+            Please enter a 10-digit phone number
+          </Form.Control.Feedback>
         </Form.Group>
         <div className="d-flex justify-content-center mb-3">
-
-        <Button variant="primary" type="submit">
-          Submit
+          <Button variant="primary" type="submit">
+            Submit
           </Button>
-          </div>
+        </div>
       </Form>
     </div>
   );
